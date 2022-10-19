9d0bd686948e48466096881eea6d91caa46acaf5
yangxdev <j.xiang@campus.unimib.it>
2022-10-19 16:43:22 +0200

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import "./App.css";
+import "./style/fonts.css";
+import "./style/vars.css";
+import React from 'react';
+
+function App() {
+  return (
+    <div className="main">
+
+    </div>
+  );
+}
+
+export default App;
