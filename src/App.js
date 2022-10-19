128de8500443aca1dea9347ecb5753cb77a8c458
yangxdev <j.xiang@campus.unimib.it>
2022-10-19 17:24:07 +0200

    backgrounds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import "./App.css";
 import "./style/fonts.css";
 import "./style/vars.css";
-import React from 'react';
+import React from "react";
+import background from "./image/bg-main-desktop.png";
 
 function App() {
   return (
     <div className="main">
-
+      <div className="backgrounds">
+        <div
+          className="background-left"
+          style={{ backgroundImage: `url(${background})` }}
+        >
+          
+        </div>
+        <div className="background-right"></div>
+      </div>
     </div>
   );
 }
